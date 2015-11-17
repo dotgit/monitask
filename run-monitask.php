62fa3b8b2d58da85476999bbc85ac6209de07dfa
Stas Trefilov <stas.trefilov@gmail.com>
2015-11-17 22:19:45 +0100

    default ini file in ini/monitask.ini

--- run-monitask.php
+++ run-monitask.php
@@ -5,7 +5,7 @@ set_include_path(__DIR__.PATH_SEPARATOR.get_include_path());
 spl_autoload_register();
 
 // check input params
-if ($argc != 3 or ! in_array($argv[1], ['--datastore', '--template', '--collect', '--export']))
+if ($argc < 2 or ! in_array($argv[1], ['--datastore', '--template', '--collect', '--export']))
 	die(
         'Usage: '.$argv[0].' [--datastore, --template, --collect, --export] [path-to-config.ini]'.PHP_EOL.
 		'where'.PHP_EOL.
@@ -17,7 +17,7 @@ if ($argc != 3 or ! in_array($argv[1], ['--datastore', '--template', '--collect'
 
 // set $OP and $Config from input
 $Op = $argv[1];
-$Config = realpath($argv[2]);
+$Config = realpath(isset($argv[2]) ? $argv[2] : (__DIR__.'/ini/monitask.ini'));
 
 if (! $Config)
     die("Config file {$argv[2]} does not exist");
