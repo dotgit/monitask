b756af8414871e32744dfd67d2ec0d2025df84b7
Stas Trefilov <stas.trefilov@gmail.com>
2015-11-27 19:29:20 +0100

    headings formatting to ensure visibility of the header when navigating from menu (padding-top 55px instead of margin-top/margin-bottom)

--- plugins/gchartstemplate.php
+++ plugins/gchartstemplate.php
@@ -4,18 +4,20 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <title><?=$Hostname?> - Monitask</title>
-<link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <style>
 body{padding-top:70px;background-color:#f8f8f8;}
+.page-header{padding-top:55px;margin-top:0;margin-bottom:0;}
+.lead{padding-top:55px;margin-top:0;margin-bottom:0;}
 </style>
 <body>
 
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container">
     <div class="pull-right">
-      <span class="navbar-text hidden-xs"><span class="hidden-xs hidden-sm">Updated:</span> <span id="<?=$Time_id?>"></span></span>&nbsp;
+      <span class="navbar-text hidden-xs"><span class="hidden-sm">Updated:</span> <span id="<?=$Time_id?>"></span></span>&nbsp;
       <button href="#" class="navbar-btn btn btn-default" onclick="update()">
-        <span class="glyphicon glyphicon-refresh" aria-hidden="true"></span>
+        <span style="font-weight:900;">&#10227;</span>
         <span class="hidden-xs">Refresh</span>
         <span class="visible-xs-inline" id="<?=$Refresh_id?>"></span>
       </button>
@@ -30,7 +32,7 @@ body{padding-top:70px;background-color:#f8f8f8;}
 <?=$Html_container?>
 </div>
 
-<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+<script type="text/javascript" src="//www.google.com/jsapi"></script>
 <script type="text/javascript">
 <?=$Js_footer?>
 </script>
