5b7cc3e64c089fdcf1662dde09a61d5a7213c60f
Stas Trefilov <stas.trefilov@gmail.com>
2015-12-04 21:21:26 +0100

    fix: TextExport::export() declaration corrected

--- plugins/textexport.php
+++ plugins/textexport.php
@@ -9,7 +9,7 @@ Class TextExport extends Export
     // export type
     const TYPE_TEXT = 'text';
 
-    public function export(array $items, array $periods, Store $store)
+    public function export(array $items, Store $store)
     {
         $info = rtrim(`uname -n`).'   '.date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']);
 
